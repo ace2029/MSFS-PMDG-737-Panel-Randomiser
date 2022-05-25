8580f03ed3fee028f13e29adc734f185fdbf125d
ace2029 <AndrewCurry2@icloud.com>
2022-05-25 16:23:20 +0100

    Initial upload of existing project to Git repository

---
+++ MSFS PMDG 737-700 Panel Randomiser/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MSFS_PMDG_737_700_Panel_Randomiser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(414, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Randomise";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(12, 110);
+            this.hScrollBar1.Maximum = 109;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(378, 26);
+            this.hScrollBar1.TabIndex = 1;
+            this.hScrollBar1.ValueChanged += new System.EventHandler(this.scrollChange);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(398, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "% chance of randomisation";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 189);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "PMDG 737-700 Panel Randomiser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
